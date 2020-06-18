3f348399aeb3330d73b428558c003a7feb0154ed
Aleksey Belchenkov <u608110@gmail.com>
2020-06-18 11:41:02 +0300

    Change Voice

--- js/scripts.js
+++ js/scripts.js
@@ -109,6 +109,10 @@ function speakText() {
     speechSynthesis.speak(message);
 }
 
+function setVoice(e) {
+    message.voice = voices.find(voice => voice.name === e.target.value);
+}
+
 // Voices changed
 speechSynthesis.addEventListener('voiceschanged', getVoices);
 
@@ -121,5 +125,8 @@ toggleBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
    document.getElementById('text-box').classList.remove('show');
 });
+
+// Change voice
+voicesSelect.addEventListener('change', setVoice);
 
 getVoices();
